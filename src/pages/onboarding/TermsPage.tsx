ef37a6de047bf6659a33f58a96e387ee78d2e69f
haneul <gkdl0113>
2026-01-29 04:47:03 +0900

    ♻️ Refactor: 약관동의 상태관리( 전체동의/개별동의 개선 )

--- src/pages/onboarding/TermsPage.tsx
+++ src/pages/onboarding/TermsPage.tsx
@@ -7,30 +7,54 @@ import Unchecked from '../../assets/icons/unchecked.svg'
 import rightarrow from '../../assets/icons/rightarrow.svg'
 import { useNavigate } from 'react-router-dom';
 import  { useState } from 'react';
-
+import { useEffect } from 'react';
 
 const TermsPage = () => {
 const navigate = useNavigate();
-const [] = useState(checkgray);
 const [active] = useState(false);
+const [allAgreed, setAllAgreed] = useState(false);
 const [require1, setRequire1] = useState(false);
 const [require2, setRequire2] = useState(false);
 const [require3, setRequire3] = useState(false);
+
+useEffect(() => {
+  if (require1 && require2 && require3) {
+    setAllAgreed(true);
+  } else {
+    setAllAgreed(false);
+  }
+}, [require1, require2, require3]);
+
 const handleNext = (path: string) => {
-  if (active) {
+  if (allAgreed) {
     navigate(path);
   } else {
     alert("약관에 동의해 주세요.");
   }
 }
 
-const allAgreed= require1 && require2 && require3;
+
 const handleAllClick = () => {
     const newState = !allAgreed;
     setRequire1(newState);
     setRequire2(newState);
     setRequire3(newState);
-}
+};
+const handleRequire1 = () => {
+  setRequire1(prev => {
+    return !prev;
+  });
+};
+const handleRequire2 = () => {
+  setRequire2(prev => {
+    return !prev;
+  });
+};
+const handleRequire3 = () => {
+  setRequire3(prev => {
+    return !prev;
+  });
+};
   
   return (
     <div className='flex flex-col w-full mt-[16px] px-4'>
@@ -51,10 +75,7 @@ const handleAllClick = () => {
         <br />
         <span className="font-semibold text-[20px] leading-[26px] text-secondary">약관 동의</span>가 필요해요 </p>
       </div>
-
-
     
-        
     <button 
         onClick={() => handleAllClick()}
         className={`justify-center border-[1.2px] rounded-[12px] w-full h-[50px] px-4 border-gray-300 flex items-center gap-[10px]
@@ -72,7 +93,7 @@ const handleAllClick = () => {
     </button>
 
     <div className="flex flex-row items-center text-left w-full mt-[30px] gap-[8px]">
-        <button onClick={require1 ? () => setRequire1(false) : () => setRequire1(true)}>
+        <button onClick={handleRequire1}>
         <img src={!require1 ? Unchecked : checkIcon} alt="" />
         </button>
         <p className=" text-[16px] font-medium text-gray-900"> (필수) 이용약관 동의</p>
@@ -80,7 +101,7 @@ const handleAllClick = () => {
     </div>
 
     <div className="flex flex-row items-center text-left w-full mt-[30px] gap-[8px]">
-        <button onClick={require2 ? () => setRequire2(false) : () => setRequire2(true)}>
+        <button onClick={handleRequire2}>
         <img src={!require2 ? Unchecked : checkIcon} alt="" />
         </button>
         <p className=" text-[16px] font-medium text-gray-900"> (필수) 개인정보 처리방침 동의</p>
@@ -88,7 +109,7 @@ const handleAllClick = () => {
     </div>
 
     <div className="flex flex-row items-center text-left w-full mt-[30px] gap-[8px]">
-        <button onClick={require3 ? () => setRequire3(false) : () => setRequire3(true)}>
+        <button onClick={handleRequire3}>
         <img src={!require3 ? Unchecked : checkIcon} alt="" />
         </button>
         <p className=" text-[16px] font-medium text-gray-900"> (선택) 마케팅 정보수신 동의</p>
@@ -96,8 +117,8 @@ const handleAllClick = () => {
     </div>
 
     <div className="w-full mt-[325px] mb-[52px]">
-        <Button disabled={!allAgreed}
-        onClick={() => { handleNext('/onboarding/persona/test'); }} >
+        <Button disabled={!(require1 && require2)}
+        onClick={() => { handleNext('/onboarding/personaresult'); }} >
           다음
         </Button>
     </div>
