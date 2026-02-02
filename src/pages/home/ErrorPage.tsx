110033b7d665cabf1ab0ed6ea4e486b7b642d1a0
nakyeong <yunna3425@naver.com>
2026-02-03 04:56:16 +0900

    ♻️ Refactor: 이미지 변수명 수정

--- src/pages/home/ErrorPage.tsx
+++ src/pages/home/ErrorPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Error from '../../assets/images/error.svg';
+import ErrorIcon from '../../assets/images/error.svg';
 import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
@@ -7,7 +7,7 @@ const ErrorPage = () => {
 
   return (
     <div className="relative flex flex-1 flex-col items-center px-4">
-      <img src={Error} alt="에러 이미지" className="mt-45 mb-[7px]" />
+      <img src={ErrorIcon} alt="에러 이미지" className="mt-45 mb-[7px]" />
       <p className="text-lg font-semibold">페이지를 찾을 수 없어요</p>
       <div className="absolute bottom-15 left-0 flex w-full flex-col gap-3 px-4">
         <button
