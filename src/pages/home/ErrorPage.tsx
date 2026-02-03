80f774f51da8bb0c616ae27a02d7a24525760060
nakyeong <yunna3425@naver.com>
2026-02-03 18:32:21 +0900

    ♻️ Refactor: 상대 경로를 @ 별칭 경로로 수정

--- src/pages/home/ErrorPage.tsx
+++ src/pages/home/ErrorPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ErrorIcon from '../../assets/images/error.svg';
+import ErrorIcon from '@/assets/images/error.svg';
 import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
