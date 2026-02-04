6e4dd322233a42f29b66fc190fdd1907e0ba0e76
haneul <gkdl0113>
2026-02-04 18:56:29 +0900

    🐛 Fix: personaTest.ts 내 client import 경로 오류 수정

--- src/types/personaTest.ts
+++ src/types/personaTest.ts
@@ -1,4 +1,4 @@
-import { api } from "../types/client";
+import { api } from "@/apis/client";
 import type { ApiResponse } from "./types";
 
 export type PersonaQuestionOption = {
--- src/utils/tokenStorage.ts
+++ src/utils/tokenStorage.ts
@@ -1,6 +1,5 @@
 const ACCESS_TOKEN_KEY = 'accessToken';
 
-// 👇 여기에 'export'가 반드시 있어야 합니다!
 export const tokenStorage = {
   get() {
     return localStorage.getItem(ACCESS_TOKEN_KEY);
