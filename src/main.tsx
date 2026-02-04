e5e705b7a110fb4084e6d6ac3cd35a638ba44bf8
rladmsgki <rladmsgki@naver.com>
2026-02-04 21:45:14 +0900

    ✨ Feat: QueryClient 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import App from '@/App';
 import { registerSW } from 'virtual:pwa-register';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 registerSW({ immediate: true });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
