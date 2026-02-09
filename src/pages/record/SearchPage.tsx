ca550e9a7e8e2194b0712ca98212a9393ce62b96
nakyeong <yunna3425@naver.com>
2026-02-10 03:50:04 +0900

    ✨ Feat: 검색어 입력 시 IME 오류 방지

--- src/pages/record/SearchPage.tsx
+++ src/pages/record/SearchPage.tsx
@@ -19,6 +19,7 @@ const SearchPage = () => {
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.isComposing) return;
     if (e.key === 'Enter') {
       handleSearch();
     }
