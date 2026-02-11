769781ec8af06d3ecc2c5f2b9a20fd6fc215f04c
nakyeong <yunna3425@naver.com>
2026-02-11 22:50:59 +0900

    🐛 Fix: 랜딩 페이지를 온보딩으로 변경

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,21 +1,15 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { tokenStorage } from "@/utils/tokenStorage";
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { tokenStorage } from '@/utils/tokenStorage';
 
 const ProtectedRoute = () => {
   const token = tokenStorage.get();
   const location = useLocation();
 
   if (!token) {
-    return (
-      <Navigate
-        to="/login"
-        replace
-        state={{ from: location }}
-      />
-    );
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
   return <Outlet />;
-}
+};
 
 export default ProtectedRoute;
